ad9aeab026e64178f750f5cf50f734f51215abe8
GSmithApps <grant.smith@temporal.io>
2025-03-22 14:14:26 -0500

    I like it

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
-"""Grant, this is just the client code"""
-
 import asyncio
 from temporalio.client import Client
+import sys
 
 from worker import (
     MonthlyCharge
@@ -9,14 +8,16 @@ from worker import (
 
 async def main():
 
+    customer_name = sys.argv[1]
+
     client = await Client.connect("localhost:7233")
 
-    await client.execute_workflow(
+    await client.start_workflow(
         MonthlyCharge.run,
-        id="hello-activity-choice-workflow-id-2",
-        task_queue="hello-activity-choice-task-queue",
+        customer_name,
+        id=f"monthly-charge-{customer_name}",
+        task_queue="monthly-charge-task-queue",
     )
-
 
 if __name__ == "__main__":
     asyncio.run(main())
--- worker.py
+++ worker.py
@@ -1,51 +1,48 @@
-"""Grant, put the workflow and activity implementations
-here along with the worker start"""
-
 import asyncio
 from datetime import timedelta
-from typing import List
+import itertools
+from dataclasses import dataclass
 
 from temporalio import activity, workflow
 from temporalio.client import Client
 from temporalio.worker import Worker
 
-# Activities that will be called by the workflow
 
+@dataclass
+class ActivityInput:
+    name: str
+    month: int
 
 @activity.defn
-async def bill_customer(month: int):
-    print(f'billing customer for month {month}')
+async def bill_customer(vals: ActivityInput):
+    print(f'billing {vals.name} for month {vals.month}')
 
 
-# Basic workflow that logs and invokes different activities based on input
 @workflow.defn
 class MonthlyCharge:
+
     @workflow.run
-    async def run(self):
-        # Order each thing on the list
-        ordered: List[str] = []
-        for month in range(1, 12+1):
-            await asyncio.sleep(4)
+    async def run(self, customer_name: str):
+
+        for month in itertools.count(1):
             await workflow.execute_activity(
                 bill_customer,
-                month,
+                ActivityInput(month=month, name=customer_name),
                 start_to_close_timeout=timedelta(seconds=5),
             )
+            await asyncio.sleep(4)
 
 
 async def main():
-    # Start client
+
     client = await Client.connect("localhost:7233")
 
-    # Run a worker for the workflow
-    worker = Worker(
+    await Worker(
         client,
-        task_queue="hello-activity-choice-task-queue",
+        task_queue="monthly-charge-task-queue",
         workflows=[MonthlyCharge],
         activities=[bill_customer],
-    )
-
-    await worker.run()
+    ).run()
 
 
 if __name__ == "__main__":
